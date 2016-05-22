9ce5b379a9d12f5388500a9d27f3277f798b0e0c
Jinme Mirabal <mirabalj@gmail.com>
2016-05-22 12:08:05 -0400

    Format date

--- app/controllers/timeStamp.server.js
+++ app/controllers/timeStamp.server.js
@@ -7,11 +7,17 @@ function Timestamp() {
   };
   
   this.isDate = function(value) {
-    return (new Date(value).toString() !== "Invalid date");
+    var date = new Date(value);
+    return (date.toString() !== 'Invalid Date');
   }
 
   this.formatDate = function(date) {
-  
+    var month = ["January", "February", "March",
+                  "April", "May", "June",
+                  "July", "August", "September",
+                  "October", "November", "December"
+    ];
+    return month[date.getMonth()] + ' ' + (date.getDate() + 1) + ", " + date.getFullYear();
   }
   
   this.getTimestamp = function(date) {
@@ -19,9 +25,9 @@ function Timestamp() {
     var unix = null;
     if (this.isNumeric(date)) {
       unix = parseInt(date);
-      natural = new Date(date * 1000);
+      natural = this.formatDate(new Date(date * 1000));
     } else if (this.isDate(date)) {
-      natural = new Date(date + " 00:00:00 GMT");
+      natural = this.formatDate(new Date(date + " 00:00:00 GMT"));
       unix = Date.parse(natural) / 1000;
     }
     return JSON.stringify({ "unix": unix, "natural": natural });
